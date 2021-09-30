9ae8c783071a1365537db713057047664a75fada
Mutsumix <mkajihara@hotmail.co.jp>
2021-09-30 16:52:51 +0900

    added the function to play music from Bluetooth

--- wc-light.py
+++ wc-light.py
@@ -24,11 +24,13 @@ def sense():
     # Hue settings
     HUE_API_OUTSIDE=settings.HUE_API_OUTSIDE
 
-    HUMAN_DURATION=45
+    LIGHT_DURATION=15
+    MUSIC_DURATION=30
 
-    count_down=HUMAN_DURATION
-    count=0
-    count_no=0
+    light_count_down=0
+    music_count_down=0
+    #count=0
+    #count_no=0
     
     try:
         
@@ -38,24 +40,34 @@ def sense():
                 
                 now = datetime.datetime.now()
                 print(now)
-                count+=1
-                count_down=HUMAN_DURATION
-                
-                print('someone is there !!: ' + str(count))
+                #count+=1
+                print('someone is there !!')
                 
                 #RED light on
                 result = requests.put(HUE_API_OUTSIDE, json = {"on":True, "bri":60, "xy":[0.7, 0.26]})
-                                            
+                #sound on
+                #sound_on("uminomierumachi", light_count_down)
+                #sound_on("chigiri", music_count_down)
+                light_count_down=LIGHT_DURATION
+                music_count_down=MUSIC_DURATION
+  
             else:
-                count_down-=1
-                print(count_down)
+                light_count_down-=1
+                music_count_down-=1
+                print('Light:' + str(light_count_down) + ', Music' + str(music_count_down))
                     
-                if count_down == 0:
-                    count_no+=1
-                    print('light turns off: ' + str(count_no))
+                if light_count_down == 0:
+                    #count_no+=1
+                    print('light turns off')
                           
                     #light off
                     result = requests.put(HUE_API_OUTSIDE, json = {"on":False})
+                                    
+                if music_count_down == 0:
+                    print('music turns off')
+                    
+                    #misic off
+                    #sound_off()
             
                 #count_no+=1
                 #print('there is no one: ' + str(count_no))
@@ -69,6 +81,16 @@ def sense():
 
 def callHueApi(color=0):
     pass
+
+def sound_on(se, count_down):
+    if count_down < 0:
+        print(count_down)
+        subprocess.Popen(['mpg321', '-g', '5', '-q', 'music/' + se + '.mp3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#    else:
+#        print('process ongoing')
+    
+def sound_off():
+    subprocess.Popen(['killall', 'mpg321'])
 
 if __name__=='__main__':        
         sense()
